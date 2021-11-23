5ed4ab21116adf0e6791529b06380acefc083d90
thzoid <thiagozoid@gmail.com>
2021-11-23 20:57:24 -0300

    Clean print function

--- main.go
+++ main.go
@@ -14,19 +14,19 @@ func BlockView(bt *blocktree.Blocktree, h blocktree.Hash) {
 		fmt.Printf("block %x\n", h)
 	}
 	for sender, tx := range block.Transactions() {
-		fmt.Printf("├tx %x\t\t ◀ %s\n", tx.Hash(), sender)
+		fmt.Printf("├─tx %x\t\t ◀ %s\n", tx.Hash(), sender)
 		txConnector := '├'
 		for j, in := range tx.Inputs {
 			if j == len(tx.Inputs)-1 && len(tx.Outputs) == 0 {
 				txConnector = '└'
 			}
-			fmt.Printf("│%c─in  %x[%d]\n", txConnector, in.ID, in.Index)
+			fmt.Printf("│ %c─in  %x[%d]\n", txConnector, in.ID, in.Index)
 		}
 		for j, out := range tx.Outputs {
 			if j == len(tx.Outputs)-1 {
 				txConnector = '└'
 			}
-			fmt.Printf("│%c─out %d\t\t ▶ %s\n", txConnector, out.Value, out.PubKey)
+			fmt.Printf("│ %c─out %d\t\t ▶ %s\n", txConnector, out.Value, out.PubKey)
 		}
 		fmt.Printf("│\n")
 	}
@@ -38,19 +38,19 @@ func main() {
 	BlockView(&tree, root)
 
 	b1 := blocktree.NewBlock(root)
-	b1.AddTx(tree, "alice", blocktree.TxOutput{PubKey: "bob", Value: 5}, blocktree.TxOutput{PubKey: "sigma", Value: 2})
-	b1Hash := tree.Graft(b1, "eve")
+	b1.AddTx(tree, "alice", blocktree.TxOutput{PubKey: "bob", Value: 5}, blocktree.TxOutput{PubKey: "dave", Value: 2})
+	b1Hash := tree.Graft(b1, "carol")
 	BlockView(&tree, b1Hash)
 
 	b2 := blocktree.NewBlock(b1Hash)
-	b2.AddTx(tree, "bob", blocktree.TxOutput{PubKey: "sigma", Value: 3})
-	b2.AddTx(tree, "eve", blocktree.TxOutput{PubKey: "sigma", Value: 2})
-	b2Hash := tree.Graft(b2, "eve")
+	b2.AddTx(tree, "bob", blocktree.TxOutput{PubKey: "dave", Value: 3})
+	b2.AddTx(tree, "carol", blocktree.TxOutput{PubKey: "dave", Value: 2})
+	b2Hash := tree.Graft(b2, "carol")
 	BlockView(&tree, b2Hash)
 
 	b3 := blocktree.NewBlock(b2Hash)
-	b3.AddTx(tree, "eve", blocktree.TxOutput{PubKey: "alice", Value: 198})
-	b3Hash := tree.Graft(b3, "eve")
+	b3.AddTx(tree, "carol", blocktree.TxOutput{PubKey: "alice", Value: 198})
+	b3Hash := tree.Graft(b3, "carol")
 	BlockView(&tree, b3Hash)
 
 	// fmt.Println("\nTree view")
